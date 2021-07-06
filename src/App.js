a9ac0ebd649ad349e8efb01220f4f821b6a9e6b2
Rahul <r27m93@gmail.com>
2021-07-06 15:00:01 +0530

    css done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{useState} from 'react';
-import './App.css';
 import Icon from './components/Icon';
 
 // toastify import
@@ -10,6 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import {Card,CardBody,Container,Button,Col,Row} from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import './App.css';
 const itemArray =new Array(9).fill("empty")
 
 const App=()=> {
@@ -18,25 +18,63 @@ const App=()=> {
   const [isCross,setIsCross]=useState(false)
   const [winMessage,setWinMessage]=useState("")
 
-  const reloadGame=()=>{
+  const reloadGame = () =>{
+    setIsCross(false)
+    setWinMessage("")
+    itemArray.fill("empty",0,9);
+  }
+  const checkIsWinner=() =>{
     //
   }
-  const checkIsWinner=()=>{
-    //
-  }
-  const changeItem=itemNumber=>{
-    //
+  const changeItem = itemNumber =>{
+    if(winMessage){
+      return toast(winMessage,{type:'success'})
+    }
+    if (itemArray[itemNumber]==="empty") {
+      itemArray[itemNumber] = isCross?"cross":"circle"
+      setIsCross(!isCross)
+
+      
+    }else{
+      return toast('already filled',{type:'error'})
+    }
+    checkIsWinner(); 
   }
 
 
   return (
-    <div className="App">
-     
-        
-        <Icon name="cross"/>
-       
+    
+    <Container className="p-5">
       
-    </div>
+      <ToastContainer position="bottom-center"/>
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-success text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={reloadGame}>Reload the Game</Button>
+            </div>
+          ):(
+            <h1 className="text-center text-warning">
+              {isCross?"Cross":"Circle"} turns
+            </h1>
+          )}
+          <div className="grid">
+            {itemArray.map((item,index)=>(
+              <Card color="warning" onClick ={()=>changeItem(index) }>
+                <CardBody className="box">
+                  <Icon name={item}/>
+                </CardBody>
+              </Card>
+            ))} 
+
+          </div>
+        
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
