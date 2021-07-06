e55b5f22cfab88dfc961a1770443e108085586b8
Rahul <r27m93@gmail.com>
2021-07-06 15:55:56 +0530

    hell its working

--- src/App.js
+++ src/App.js
@@ -23,9 +23,57 @@ const App=()=> {
     setWinMessage("")
     itemArray.fill("empty",0,9);
   }
-  const checkIsWinner=() =>{
-    //
+  const checkIsWinner = () =>{
+    if(itemArray[0]===itemArray[1] &&
+      itemArray[0]===itemArray[2] &&
+      itemArray[0]!=="empty"){
+        setWinMessage(`${itemArray[0]} wins`)
+      } else if(
+        itemArray[3]===itemArray[4] &&
+        itemArray[3]===itemArray[5]&&
+        itemArray[3]!=="empty"
+      ){
+        setWinMessage(`${itemArray[3]} wins`)
+      } else if(
+        itemArray[6]===itemArray[7] &&
+        itemArray[6]===itemArray[8] &&
+        itemArray[6]!=="empty"
+      ){
+        setWinMessage(`${itemArray[6]} wins`)
+      }else if(
+        itemArray[0]===itemArray[3] &&
+        itemArray[0]===itemArray[6]&&
+        itemArray[0]!=="empty"
+      ){
+        setWinMessage(`${itemArray[0]} wins`)
+      }else if(
+        itemArray[1]===itemArray[4] &&
+        itemArray[1]===itemArray[7]&&
+        itemArray[1]!=="empty"
+      ){
+        setWinMessage(`${itemArray[1]} wins`)
+      }else if(
+        itemArray[2]===itemArray[5] &&
+        itemArray[2]===itemArray[8]&&
+        itemArray[2]!=="empty"
+      ){
+        setWinMessage(`${itemArray[2]} wins`)
+      }else if(
+        itemArray[0]===itemArray[4] &&
+        itemArray[0]===itemArray[8]&&
+        itemArray[0]!=="empty"
+      ){
+        setWinMessage(`${itemArray[3]} wins`)
+      }else if(
+        itemArray[2]===itemArray[4] &&
+        itemArray[2]===itemArray[6]&&
+        itemArray[2]!=="empty"
+      ){
+        setWinMessage(`${itemArray[2]} wins`)
+      }
   }
+
+
   const changeItem = itemNumber =>{
     if(winMessage){
       return toast(winMessage,{type:'success'})
